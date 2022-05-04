378d0731b48219a8ca84cb8f1b5e73641d9a5d66
Richard Martikan <ric.martikan@gmail.com>
2022-05-04 23:05:52 +0200

    Fixing the logging in crypto_utils

--- util/crypto_utils/crypto_utils.go
+++ util/crypto_utils/crypto_utils.go
@@ -2,6 +2,7 @@ package crypto_utils
 
 import (
 	"github.com/martikan/bookstore_users-api/errors"
+	"github.com/martikan/bookstore_users-api/logger"
 	"golang.org/x/crypto/bcrypt"
 )
 
@@ -10,7 +11,8 @@ func HashPassword(input string) (string, *errors.RestError) {
 
 	hash, err := bcrypt.GenerateFromPassword([]byte(input), bcrypt.DefaultCost)
 	if err != nil {
-		return "", errors.NewInternalServerError(err.Error())
+		logger.Error("Error when trying to hash the password", err)
+		return "", errors.NewInternalServerError("Api error")
 	}
 
 	return string(hash), nil
